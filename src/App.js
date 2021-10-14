10a3cb336469a78240689ecad805276ee4881598
Luan Felipe <luan0412@outlook.com>
2021-10-14 16:50:10 -0300

    Primeira aula de React

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Aprendendo React</h1>
+      <img src="https://www.iguiecologia.com/wp-content/uploads/2021/02/Coruja-buraqueira7-2.jpg" alt="corujita">
+      </img>
+
+      <p>
+        <pre>
+          Os gregos diziam que a coruja é o símbolo da sabedoria porque consideravam a noite
+            como o momento da revelação intelectual e do pensamento filosófico,
+            e a coruja, por ter hábitos noturnos, acabou representando a busca pelo saber.
+            Por causa disso, a deusa Atena tinha uma coruja como mascote.
+        </pre>
+      </p>
+
+    </div>
+  );
+}
+
+export default App;
